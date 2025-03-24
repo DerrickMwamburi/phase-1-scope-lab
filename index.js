1b8fda34568f167a7e79b9ae76cacbeb9d19d8d5
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-03-24 14:58:06 +0300

    code done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a variable in global scope called customerName using the var keyword
+var customerName = 'bob';
+
+// Function that accesses and uppercases the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function that declares a global bestCustomer variable
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // This becomes global because it's not declared with var/let/const
+}
+
+// Function that changes the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Function that attempts to change the constant (which will throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw a TypeError
+}
